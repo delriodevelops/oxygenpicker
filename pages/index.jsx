a0953dc59d3f4a5814186a5e54ca209a1d5a5b1a
Christian del Rio Rodriguez <delriodeveloper@gmail.com>
2022-08-19 20:06:28 +0200

    styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import {CompactPicker} from 'react-color'
-
+import {BiTrash}from 'react-icons/bi'
 const defaultBg = 'linear-gradient(130.53deg, #404040 17.47%, #404040 27.71%, #B0B0B0 27.72%, #B0B0B0 36.75%, #404040 36.76%, #404040 45.49%, #B0B0B0 45.5%, #B0B0B0 54.23%, #404040 54.24%, #404040 63.27%, #B0B0B0 63.27%, #B0B0B0 71.71%, #404040 71.71%)'
 
 export default function Home() {
@@ -66,41 +66,48 @@ export default function Home() {
 
 
   return (
-    <>
+    <div className={styles.root}>
       <header>
         Color palette generator
       </header>
 
-      <div>
+      <div className={styles.circlesWrapper}>
         <div className={styles.circleHoldr}>
           {palete.map((element,index)=>
             <span onClick={e=>{handleClick(index,e)}} key={index} id={`iptCircle${index}`} className={`iptCircle ${active===index && 'active'}`} >{palete[index]===defaultBg && '+'}</span>
           )}
         </div>
       </div>
-      <div>
+      <div className={styles.formWrapper}>
         <CompactPicker onChange={fillCircle}/>
-        <form onSubmit={savePalete}>
-          <label htmlFor="name"/>
-          <input type="text" name="name" placeholder='Website color scheme' value={iptValue} onChange={(e)=>setIptValue(e.target.value)}/>
-          <input type="submit" value="+" />
+        <form className={styles.form} onSubmit={savePalete}>
+            <label htmlFor="name">Name</label>
+          <span className={styles.field}>
+            <input className={styles.nameIpt} type="text" name="name" placeholder='Website color scheme' value={iptValue} onChange={(e)=>setIptValue(e.target.value)}/>
+          <input type="submit" value="+" className={styles.submitBtn}/>
+          </span>
         </form>
       </div>
 
-      <section>
+      <section className={styles.paletesContainer}>
+        <h3 className={styles.containerTitle}>Saved Pallettes</h3>
+        <span className={styles.paletes}>
+
         {favs.map(({name,savedPalete},index)=>
-          <div key={index}>
-          <span>{name}</span>
-          <span onClick={()=>{deletePalete(index)}}> x</span>
-          <div className='savedPalete' onClick={()=>{displayPalete(savedPalete)}}>
+          <div className={styles.paleteInfo} key={index}>
+            <div className={styles.nameTrash}>
+              <span>{name}</span>
+              <BiTrash className={styles.trash} onClick={()=>{deletePalete(index)}}/>
+            </div>
+
+          <div className={styles.savedPalete} onClick={()=>{displayPalete(savedPalete)}}>
             {savedPalete.map((color,index)=>
               <span className='savedCircle' style={{backgroundColor:color}} key={index}></span>
-            )}
+              )}
           </div>
         </div>
-          
-        
         )}
+        </span>
       </section>
 
       
@@ -136,10 +143,7 @@ export default function Home() {
             width:100px;
             height:100px;
           }
-          .savedPalete{
-            display:flex;
-            gap:4px;
-          }
+          
           .savedCircle {
             width:40px;
             height:40px;
@@ -147,8 +151,20 @@ export default function Home() {
             border-radius:50%;
           }
 
+          @media (max-width:890px) {
+            .iptCircle{
+              width:50px;
+              height: 50px;
+              font-size:24px;
+            }
+            .active {
+              width:60px;
+              height:60px;
+            }
+          }
+
         `}
       </style>
-    </>
+    </div>
   )
 }
